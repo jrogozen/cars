06eeec88d3140ac9ece8e2204bb80fb13ed6af15
Developer <jon@readyartwork.com>
2014-07-21 10:29:37 -0700

    add total car count and car color hash

--- cars.rb
+++ cars.rb
@@ -1,11 +1,31 @@
 class Car
+	@@total_car_count = 0
+	#hash to keep track of {key: color, value: count}
+	@@cars_per_color = {} 
+
+	def self.total_car_count 
+		@@total_car_count
+	end
+
+	def self.cars_per_color
+		p @@cars_per_color
+	end
+
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize(color)
 		@fuel = 10
 		@distance = 0
+		@@total_car_count += 1
+
+		#update class variable hash's color count
+		if @@cars_per_color.has_key?(color)
+			@@cars_per_color[color] += 1
+		else
+			@@cars_per_color[color] = 1
+		end
 	end
 
 	def drive(miles)
@@ -26,8 +46,9 @@ class Car
 	end
 end
 
-car_a = Car.new()
-car_b = Car.new()
+car_a = Car.new("red")
+car_b = Car.new("blue")
+car_c = Car.new("blue")
 puts car_a
 puts car_b
 car_a.drive(10)
@@ -37,3 +58,5 @@ car_a.drive(232)
 car_b.drive(117)
 puts car_a
 puts car_b
+
+Car.cars_per_color 
