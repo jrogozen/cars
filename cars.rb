9972af9b16dc68f4eef0b7dc3f456acc9f13a397
Developer <jon@readyartwork.com>
2014-07-21 14:53:01 -0700

    add convertible if/else statements

--- cars.rb
+++ cars.rb
@@ -11,21 +11,77 @@ class Car
 		p @@cars_per_color
 	end
 
+	def self.most_popular_color
+		max_color_count = 0
+		max_color_car = ""
+		@@cars_per_color.each do |color, count|
+			if count > max_color_count
+				max_color_count = count
+				max_color_car = color
+			end
+		end
+		puts "The most popular car color is #{max_color_car}."
+	end
+
+	def color=(new_color)
+		#remove initial color from hash count
+		@@cars_per_color[@color] -= 1
+		@color = new_color
+		if @@cars_per_color.has_key?(@color)
+			@@cars_per_color[@color] += 1
+		else
+			@@cars_per_color[@color] = 1
+		end
+	end
+
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize(color)
+	def initialize(color="black")
 		@fuel = 10
 		@distance = 0
 		@@total_car_count += 1
+		@color = color
+
+		#cars are not convertibles by default
+		@convertible = false
+		@roof_status = "closed"
 
 		#update class variable hash's color count
-		if @@cars_per_color.has_key?(color)
-			@@cars_per_color[color] += 1
+		if @@cars_per_color.has_key?(@color)
+			@@cars_per_color[@color] += 1
 		else
-			@@cars_per_color[color] = 1
+			@@cars_per_color[@color] = 1
 		end
+	end
+
+	#make a car a convertible
+	def convertible
+		@convertible = true
+		puts "This car is now a convertible!"
+	end
+
+	#set the top down
+	def top_down
+		if @convertible
+			@roof_status = "open"
+		else
+			puts "This car is not a convertible."
+		end
+	end
+
+	#put the top up
+	def close_top
+		if @convertible
+			@roof_status = "closed"
+		else
+			puts "This car is not a convertible."
+		end
+	end
+
+	def top_status
+		puts "The top is #{@roof_status}."
 	end
 
 	def drive(miles)
@@ -60,3 +116,14 @@ puts car_a
 puts car_b
 
 Car.cars_per_color 
+
+car_a.color = "blue"
+Car.cars_per_color
+Car.most_popular_color
+
+car_a.top_down #=> should error
+car_a.convertible #=> this car is now a convertible!
+car_a.top_down
+car_a.top_status
+car_a.close_top
+car_a.top_status
